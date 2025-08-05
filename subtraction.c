8ce35b2e45df51fbf9fc4f89339fef061ff6c31a
Aejaz Ahamad khan <2203009@ritindia.edu>
2025-08-05 15:45:50 +0530

    This is subtraction program

---
+++ subtraction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 25;
+    int num2 = 10;
+    int difference;
+
+    difference = num1 - num2; // Subtracts num2 from num1
+
+    printf("The difference is: %d\n", difference); // Output: The difference is: 15
+
+    return 0;
+}
